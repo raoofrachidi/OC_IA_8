7324ec81325e43fb779a6a127b81230bfe3ef748
raoofrachidi <43079354+raoofrachidi@users.noreply.github.com>
2022-03-25 22:41:05 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,38 +1,33 @@
 import numpy as np
-import random, six
+import random
+import six
 import cv2
 
-class_colors = [(random.randint(0, 255), random.randint(
-    0, 255), random.randint(0, 255)) for _ in range(5000)]
+class_colors = [(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)) for _ in range(5000)]
 
 
 # Function to predict
-def infer(
-        model=None, inp=None, out_fname=None, n_classes=None, colors=class_colors,
-        prediction_width=None, prediction_height=None, read_image_type=1):
+def infer(model=None, inp=None, out_fname=None, n_classes=None, prediction_width=None, prediction_height=None):
     assert (inp is not None)
     assert ((type(inp) is np.ndarray) or isinstance(inp, six.string_types)), \
         "Input should be the CV image or the input file name"
 
     if isinstance(inp, six.string_types):
-        inp = cv2.imread(inp, read_image_type)
+        inp = cv2.imread(inp, 1)
 
     assert (len(inp.shape) == 3 or len(inp.shape) == 1 or len(inp.shape) == 4), "Image should be h,w,3 "
 
-    output_width = 256
-    output_height = 128
-    input_width = prediction_width
-    input_height = prediction_height
-    n_classes = n_classes
-
-    x = get_image_array(inp, input_width, input_height,
-                        ordering='channels_last')
+    x = get_image_array(inp, prediction_width, prediction_height)
     pr = model.predict(np.array([x]))[0]
-    pr = pr.reshape((output_height, output_width, n_classes)).argmax(axis=2)
+    pr = pr.reshape((128, 256, n_classes)).argmax(axis=2)
 
-    seg_img = visualize_segmentation(pr, inp, n_classes=n_classes, colors=colors,
-                                     prediction_width=prediction_width,
-                                     prediction_height=prediction_height)
+    seg_img = visualize_segmentation(
+        pr,
+        inp,
+        n_classes=n_classes,
+        prediction_width=prediction_width,
+        prediction_height=prediction_height
+    )
 
     if out_fname is not None:
         cv2.imwrite(out_fname, seg_img)
@@ -40,19 +35,16 @@ def infer(
     return pr
 
 
-def get_image_array(
-        image_input, width, height, imgNorm="sub_mean", ordering='channels_last', read_image_type=1):
+def get_image_array(image_input, width, height):
     """ Load image array from input """
-
+    img = None
     if type(image_input) is np.ndarray:
         # It is already an array, use it as it is
         img = image_input
     elif isinstance(image_input, six.string_types):
-        img = cv2.imread(image_input, read_image_type)
+        img = cv2.imread(image_input, 1)
 
-    if imgNorm == "sub_and_divide":
-        img = np.float32(cv2.resize(img, (width, height))) / 127.5 - 1
-    elif imgNorm == "sub_mean":
+    if img is not None:
         img = cv2.resize(img, (width, height))
         img = img.astype(np.float32)
         img = np.atleast_3d(img)
@@ -63,38 +55,27 @@ def get_image_array(
             img[:, :, i] -= means[i]
 
         img = img[:, :, ::-1]
-    elif imgNorm == "divide":
-        img = cv2.resize(img, (width, height))
-        img = img.astype(np.float32)
-        img = img / 255.0
-
-    if ordering == 'channels_first':
-        img = np.rollaxis(img, 2, 0)
     return img
 
 
-def visualize_segmentation(seg_arr, inp_img=None, n_classes=None, colors=class_colors,
-                           prediction_width=None, prediction_height=None):
+def visualize_segmentation(seg_arr, inp_img=None, n_classes=None, prediction_width=None, prediction_height=None):
     if n_classes is None:
         n_classes = np.max(seg_arr)
 
-    seg_img = get_colored_segmentation_image(seg_arr, n_classes, colors=colors)
+    seg_img = get_colored_segmentation_image(seg_arr, n_classes)
 
     if inp_img is not None:
         original_h = inp_img.shape[0]
         original_w = inp_img.shape[1]
         seg_img = cv2.resize(seg_img, (original_w, original_h), interpolation=cv2.INTER_NEAREST)
 
-    if (prediction_height is not None) and (prediction_width is not None):
+    if prediction_height is not None and prediction_width is not None:
         seg_img = cv2.resize(seg_img, (prediction_width, prediction_height), interpolation=cv2.INTER_NEAREST)
-        if inp_img is not None:
-            inp_img = cv2.resize(inp_img,
-                                 (prediction_width, prediction_height))
 
     return seg_img
 
 
-def get_colored_segmentation_image(seg_arr, n_classes, colors=class_colors):
+def get_colored_segmentation_image(seg_arr, n_classes):
     output_height = seg_arr.shape[0]
     output_width = seg_arr.shape[1]
 
@@ -102,8 +83,8 @@ def get_colored_segmentation_image(seg_arr, n_classes, colors=class_colors):
 
     for c in range(n_classes):
         seg_arr_c = seg_arr[:, :] == c
-        seg_img[:, :, 0] += ((seg_arr_c) * (colors[c][0])).astype('uint8')
-        seg_img[:, :, 1] += ((seg_arr_c) * (colors[c][1])).astype('uint8')
-        seg_img[:, :, 2] += ((seg_arr_c) * (colors[c][2])).astype('uint8')
+        seg_img[:, :, 0] += (seg_arr_c * (class_colors[c][0])).astype('uint8')
+        seg_img[:, :, 1] += (seg_arr_c * (class_colors[c][1])).astype('uint8')
+        seg_img[:, :, 2] += (seg_arr_c * (class_colors[c][2])).astype('uint8')
 
     return seg_img
